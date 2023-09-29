2b723d371138f692e53a0a01b4d907dc655103b7
ETI - Douglas Rodrigues Ferreira Campos <drcampos@sescpe.com.br>
2023-09-29 15:24:42 -0300

    TODO DIA É UM DIA PARA APRENDER

--- 01-JavaScript/ExerciciosJS/Fundamentos/Organização.js
+++ 01-JavaScript/ExerciciosJS/Fundamentos/Organização.js
@@ -3,7 +3,7 @@ console.log("Sentenças de códigos");
 {
     {
         console.log("Olá Douglas");
-        console.log("Douglas piroka gigante");
+        console.log("Douglas Rodrigues Ferreira Campos");
         console.log(4 + 5);
     }
 }
